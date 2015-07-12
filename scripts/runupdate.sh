8a15b2cb093140f3a02bee2a98698016d121a267
Quinton Reeves <qreeves@gmail.com>
2015-07-12 19:01:14 +1000

    Update runupdate.sh

--- scripts/runupdate.sh
+++ scripts/runupdate.sh
@@ -12,17 +12,23 @@ RE_INT="false"
 echo "checking master server.."
 curl --fail --max-time 3 http://play.redeclipse.net:28800/version || (RE_INT="true"; killall -INT redeclipse_server_linux)
 
-echo "update (have: ${RE_CURVER} want: ${RE_RUNVER})"
-if [ -n "${RE_RUNVER}" ] && [ "${RE_CURVER}" != "${RE_RUNVER}" ]; then
-  if [ "${RE_INT}" = "true" ]; then
-    echo "restarting master.."
-    killall -TERM redeclipse_server_linux &
+if [ ! -e "${HOME}/.runupdate" ]; then
+  echo "update (have: ${RE_CURVER} want: ${RE_RUNVER})"
+  if [ -n "${RE_RUNVER}" ] && [ "${RE_CURVER}" != "${RE_RUNVER}" ]; then
+    date -u > "${HOME}/.runupdate"
+    if [ "${RE_INT}" = "true" ]; then
+      echo "restarting master.."
+      killall -TERM redeclipse_server_linux &
+    fi
+    while [ "${RE_CURVER}" != "${RE_RUNVER}" ]; do
+      sleep 10
+      RE_CURVER=`cat "${HOME}/redeclipse-master/bin/version.txt"`
+    done
+    echo "restarting elara.."
+    killall -TERM redeclipse_elara_linux &
+    rm -f "${HOME}/.runupdate"
   fi
-  while [ "${RE_CURVER}" != "${RE_RUNVER}" ]; do
-    sleep 10
-    RE_CURVER=`cat "${HOME}/redeclipse-master/bin/version.txt"`
-  done
-  echo "restarting elara.."
-  killall -TERM redeclipse_elara_linux &
+else
+  echo "waiting for previous update.."
 fi
 echo "done."
