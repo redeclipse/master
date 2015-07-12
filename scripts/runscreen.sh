accbf3f6255e626457dd84a75291b537a12b8b63
Quinton Reeves <qreeves@gmail.com>
2015-07-12 09:43:32 -0400

    more verbosity

--- scripts/runscreen.sh
+++ scripts/runscreen.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 date -u
-echo "running screens"
+echo "running screens.."
 screen -S redeclipse -t 0 -A -d -m
 screen -S redeclipse -X screen -t 1
+RE_CURVER=`cat "${HOME}/redeclipse-master/bin/version.txt"`
+RE_RUNVER=`cat /webspace/redeclipse.net/files/stable/bins.txt`
+echo "opening master screen.."
 screen -S redeclipse -p 0 -X stuff $'. "${HOME}/runserver.sh" master\n'
-sleep 60
+echo "waiting for update.."
+while [ "${RE_CURVER}" != "${RE_RUNVER}" ]; do
+  sleep 10
+  RE_CURVER=`cat "${HOME}/redeclipse-master/bin/version.txt"`
+done
+echo "opening elara screen.."
 screen -S redeclipse -p 1 -X stuff $'. "${HOME}/runserver.sh" elara\n'
 echo "done."
