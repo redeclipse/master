5a4ca48b70b06f9b71d4f68f6a89c8ad2fbc3a0f
Quinton Reeves <qreeves@gmail.com>
2015-07-12 05:17:17 -0400

    logtimelocal 0

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -5,7 +5,7 @@ case "$1" in
   master)
     pushd "${HOME}/redeclipse-master"
     while [ 0 != 1 ]; do
-      REDECLIPSE_BRANCH=stable REDECLIPSE_HOME=home/master REDECLIPSE_BINARY=redeclipse_server ./redeclipse.sh 2>&1 | tee "${HOME}/logs/master.log"
+      REDECLIPSE_BRANCH=stable REDECLIPSE_HOME=home/master REDECLIPSE_BINARY=redeclipse_server ./redeclipse.sh "-xlogtimelocal 0" -g 2>&1 | tee "${HOME}/logs/master.log"
       sleep 10
     done
     popd
@@ -14,7 +14,7 @@ case "$1" in
     pushd "${HOME}/redeclipse-master"
     while [ 0 != 1 ]; do
       cp -v bin/amd64/redeclipse_server_linux bin/amd64/redeclipse_elara_linux
-      REDECLIPSE_BRANCH=inplace REDECLIPSE_HOME=home/elara REDECLIPSE_BINARY=redeclipse_elara ./redeclipse.sh 2>&1 | tee "${HOME}/logs/elara.log"
+      REDECLIPSE_BRANCH=inplace REDECLIPSE_HOME=home/elara REDECLIPSE_BINARY=redeclipse_elara ./redeclipse.sh "-xlogtimelocal 0" -g 2>&1 | tee "${HOME}/logs/elara.log"
       sleep 10
     done
     popd
