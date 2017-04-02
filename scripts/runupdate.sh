af44a4fc4f9da8a99c525481e4aed131840e9672
Quinton Reeves <qreeves@gmail.com>
2017-04-03 09:23:25 +1000

    fix wrong dir

--- scripts/runupdate.sh
+++ scripts/runupdate.sh
@@ -36,7 +36,7 @@ if curl --connect-timeout 30 -L -k -f "http://play.redeclipse.net:28800/version"
   for i in ${RE_RUN_SERVERS}; do
     echo "${i}: checking.."
     RE_CUR_VER=`cat "${HOME}/redeclipse-${i}/bin/version.txt"`
-    RE_CUR_BRN=`cat "${HOME}/redeclipse-${i}/bin/branch.txt"`
+    RE_CUR_BRN=`cat "${HOME}/redeclipse-${i}/branch.txt"`
     RE_RUN_VER=`curl --connect-timeout 30 -L -k -f "https://raw.githubusercontent.com/red-eclipse/deploy/master/${RE_CUR_BRN}/bins.txt"`
     echo "${i}: ${RE_CUR_VER} -> ${RE_RUN_VER}"
     if [ -n "${RE_RUN_VER}" ] && [ "${RE_CUR_VER}" != "${RE_RUN_VER}" ]; then
