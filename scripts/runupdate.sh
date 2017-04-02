1726c3062268b8d784f9b017095ad9bf5ba9b303
Quinton Reeves <qreeves@gmail.com>
2017-04-03 09:17:37 +1000

    get the branch from branch.txt nice a neatly

--- scripts/runupdate.sh
+++ scripts/runupdate.sh
@@ -32,11 +32,12 @@ fi
 
 cd "${HOME}" 2>&1 >/dev/null
 echo "checking servers.."
-if curl --connect-timeout 30 -L -k -f http://play.redeclipse.net:28800/version; then
+if curl --connect-timeout 30 -L -k -f "http://play.redeclipse.net:28800/version"; then
   for i in ${RE_RUN_SERVERS}; do
     echo "${i}: checking.."
     RE_CUR_VER=`cat "${HOME}/redeclipse-${i}/bin/version.txt"`
-    RE_RUN_VER=`curl --connect-timeout 30 -L -k -f "https://raw.githubusercontent.com/red-eclipse/deploy/master/stable/bins.txt"`
+    RE_CUR_BRN=`cat "${HOME}/redeclipse-${i}/bin/branch.txt"`
+    RE_RUN_VER=`curl --connect-timeout 30 -L -k -f "https://raw.githubusercontent.com/red-eclipse/deploy/master/${RE_CUR_BRN}/bins.txt"`
     echo "${i}: ${RE_CUR_VER} -> ${RE_RUN_VER}"
     if [ -n "${RE_RUN_VER}" ] && [ "${RE_CUR_VER}" != "${RE_RUN_VER}" ]; then
       RE_PID=`pgrep -f "redeclipse-${i}"`
