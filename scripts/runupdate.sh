f62d6812538e110b5babd6fa4ba611b8d99b03f6
Quinton Reeves <qreeves@gmail.com>
2019-09-03 07:10:28 +1000

    GitHub move from red-eclipse to redeclipse

--- scripts/runupdate.sh
+++ scripts/runupdate.sh
@@ -44,7 +44,7 @@ if curl --connect-timeout 30 -L -k -f "http://play.redeclipse.net:28800/version"
     echo "${i}: checking.."
     RE_CUR_VER=`cat "${HOME}/redeclipse-${i}/bin/version.txt"`
     RE_CUR_BRN=`cat "${HOME}/redeclipse-${i}/branch.txt"`
-    RE_RUN_VER=`curl --connect-timeout 30 -L -k -f "https://raw.githubusercontent.com/red-eclipse/deploy/master/${RE_CUR_BRN}/bins.txt"`
+    RE_RUN_VER=`curl --connect-timeout 30 -L -k -f "https://raw.githubusercontent.com/redeclipse/deploy/master/${RE_CUR_BRN}/bins.txt"`
     echo "${i}: ${RE_CUR_VER} -> ${RE_RUN_VER}"
     if [ -n "${RE_RUN_VER}" ] && [ "${RE_CUR_VER}" != "${RE_RUN_VER}" ]; then
       RE_PID=`pgrep -f "redeclipse-${i}"`
