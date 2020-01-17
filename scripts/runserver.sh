fa29930d781da34812b06d54d61db3b143588283
Quinton Reeves <qreeves@gmail.com>
2020-01-18 03:41:14 +1000

    update bins before running server

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -5,6 +5,8 @@ case "$1" in
   official|dev)
     cd "${HOME}/redeclipse-${1}"
     while true; do
+      rm -vf "../bins.tar.gz"
+      curl -o "../bins.tar.gz" "https://github.com/redeclipse/deploy/raw/master/master/linux.tar.gz" && tar -zxvf "../bins.tar.gz"
       REDECLIPSE_HOME="${HOME}/master/${1}" REDECLIPSE_BINARY=redeclipse_server ./redeclipse.sh -sg0 -g 2>&1 | tee --append "${HOME}/logs/server-${1}.log"
       sleep 10
     done
